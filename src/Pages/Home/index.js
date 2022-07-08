48ac0f785b0a204e25b725c49c244d444939c9f4
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-07-07 22:31:37 -0300

    Inserting data ready

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,52 +1,49 @@
 import firebase from '../../firebaseConnection';
 import React, { useEffect, useState } from 'react';
 
-function Home(){
+function Home() {
 
-    const [frases,setFrases] = useState([]);
+    const [frases, setFrases] = useState([]);
 
     useEffect(() => {
-        async function readRegister(){
-    
+        async function readRegister() {
+
             await firebase.firestore().collection('tb_frases')
-            .get()
-            .then((snapshot) => {
-                let lista = [];
-    
-                snapshot.forEach((doc) => {
-                    lista.push({
-                        id: doc.id,
-                        frase: doc.data().frases,
-                        categoria: doc.data().categoria,
-                        qtdUsos: doc.data().qtdUsos
+                .get()
+                .then((snapshot) => {
+                    let lista = [];
+
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id: doc.id,
+                            frase: doc.data().frase,
+                            categoria: doc.data().categoria,
+                            qtdUsos: doc.data().qtdUsos
+                        });
                     });
-                });
-                setFrases(lista);
-            })
-            .then(() => {
-                console.log('Lido com sucesso...');
-            })
-            .catch(() => {
-                alert('Falha ao ler o registro.');
-            })
+                    setFrases(lista);
+                })
+                .then(() => {
+                    console.log('Lido com sucesso...');
+                })
+                .catch(() => {
+                    alert('Falha ao ler o registro.');
+                })
         }
 
         readRegister();
 
     }, []);
 
-
-
-
-    return(
+    return (
         <>
             <h1>Frases</h1>
             {frases.map((item) => {
-                return(
+                return (
                     <div>
                         <span key={item.id}>Frase: </span>
-                        <span>{item.frase} - usadas {item.qtdUsos} vezes.</span>
-                        <br/>
+                        <span>{item.frase} - usadas <strong>{item.qtdUsos}</strong> vezes.</span>
+                        <br />
                     </div>
                 )
             })
