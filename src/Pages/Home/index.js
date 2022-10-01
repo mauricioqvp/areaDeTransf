72b99e87223e1015b6886c2f49e9edaac1c71de8
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-10-01 00:44:02 -0300

    Esteticas

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -7,7 +7,7 @@ import './styles.css';
 function Home() {
 
     const [frases, setFrases] = useState([]);
-    const [categoria, setCategoria] = useState('Todas as categorias');
+    const [categoria, setCategoria] = useState('Escolha a categoria');
 
     useEffect(() => {
         async function readRegister() {
@@ -15,7 +15,7 @@ function Home() {
             await firebase.firestore().collection('tb_frases')
                 .get()
                 .then((snapshot) => {
-                    let lista = [];
+                    var lista = [];
 
                     snapshot.forEach((doc) => {
                         lista.push({
@@ -23,59 +23,61 @@ function Home() {
                             frase: doc.data().frase,
                             categoria: doc.data().categoria,
                             qtdUsos: doc.data().qtdUsos
-                        });
+                        })
                     });
-                    lista.sort((a,b) => (a.qtdUsos > b.qtdUsos) ? -1 : ((b.qtdUsos > a.qtdUsos) ? 1 : 0));
+
+                    lista.sort((a, b) => (a.qtdUsos > b.qtdUsos) ? -1 : ((b.qtdUsos > a.qtdUsos) ? 1 : 0));
                     setFrases(lista);
                 })
                 .then(() => {
                     console.log('Lido com sucesso...');
+                    console.log(categoria);
                 })
                 .catch(() => {
                     alert('Falha ao ler o registro.');
                 })
-            }
-            
-            readRegister();
-            
-        }, []);
+        }
+
+        readRegister();
+
+    }, [categoria]);
 
     async function copiaIndice(id, frase, categoria, usos) {
         navigator.clipboard.writeText(frase);
         toast.success("Área de transferência");
 
-                await firebase.firestore().collection('tb_frases')
-                .doc(id)
-                .set({
-                    id: id,
-                    frase: frase,
-                    categoria: categoria,
-                    qtdUsos: usos + 1
-                })
-                .then(() => {
-                    console.log('Gravada a nova qtdUsos na frase escolhida');
-                });
+        await firebase.firestore().collection('tb_frases')
+            .doc(id)
+            .set({
+                id: id,
+                frase: frase,
+                categoria: categoria,
+                qtdUsos: usos + 1
+            })
+            .then(() => {
+                console.log('Gravada a nova qtdUsos na frase escolhida');
+            });
     }
 
     async function atualizaConteudo(id, frase) {
         navigator.clipboard.writeText(frase);
         toast.success("Área de transferência");
-                
-                // corrigir esta rotina para update deste conteúdo
-                await firebase.firestore().collection('tb_frases')
-                .doc(id)
-                .set({
-                    id: id,
-                    frase: frase,
-                })
-                .then(() => {
-                    console.log('Não está fazendo a tarefa correta.');
-                });
+
+        // corrigir esta rotina para update deste conteúdo
+        await firebase.firestore().collection('tb_frases')
+            .doc(id)
+            .set({
+                id: id,
+                frase: frase,
+            })
+            .then(() => {
+                console.log('Não está fazendo a tarefa correta.');
+            });
     }
 
-    function handleChangeSelect(e){
+    function handleChangeSelect(e) {
         setCategoria(e.target.value);
-    }   
+    }
 
     return (
         <div className='container'>
@@ -83,7 +85,7 @@ function Home() {
                 <div className='titulo-esquerdo'></div>
                 <h1 className='titulo'>Frases</h1>
                 <div className='titulo-direito'>
-                <select value={categoria} onChange={handleChangeSelect} name="categoria" className="frase-input">
+                    <select value={categoria} onChange={handleChangeSelect} name="categoria" className="frase-input">
                         <option value={categoria}>{categoria}</option>
                         <option value="Pacotes">Pacotes</option>
                         <option value="Contas">Contas</option>
@@ -103,16 +105,19 @@ function Home() {
                         <div key={item.id}>
                             <div className='frase-container'>
                                 <div><strong>{item.qtdUsos} - {item.categoria}</strong></div>
-                                <textarea className='textarea-container' defaultValue={item.frase}></textarea><br />
+                                <textarea
+                                    className='textarea-container'
+                                    value={`${categoria}` === "Escolha a categoria" ? item.frase : item.frase}>
+                                </textarea><br />
                             </div>
                             <div className='buttons-container'>
-                                <button 
-                                    onClick={() => copiaIndice(item.id, item.frase, item.categoria, item.qtdUsos)} 
+                                <button
+                                    onClick={() => copiaIndice(item.id, item.frase, item.categoria, item.qtdUsos)}
                                     alt="Copia texto para a área de trabalho">
                                     Copiar
                                 </button>
-                                <button 
-                                    onClick={() => atualizaConteudo(item.id, item.frase)} 
+                                <button
+                                    onClick={() => atualizaConteudo(item.id, item.frase)}
                                     alt="Atualizar com este conteúdo.">
                                     Editar
                                 </button>
